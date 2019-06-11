ce27c8f00e7d66160af008a1f5461761573c9f8e
Rob Fentress <learn@vt.edu>
2019-06-11 09:10:40 -0400

    initial commit

---
+++ alert_files/timed-alert.js
@@ -0,0 +1,33 @@
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ */
+window.addEventListener('load', function() {
+    this.setTimeout(addAlert, 60000);
+    // var button = document.getElementById('alert-trigger');
+
+    // button.addEventListener('click', addAlert);
+
+});
+
+/*
+ * @function addAlert
+ *
+ * @desc Adds an alert to the page
+ *
+ * @param   {Object}  event  -  Standard W3C event object
+ *
+ */
+
+function addAlert(event) {
+    var title = document.getElementById('title');
+    var desc = document.getElementById('desc');
+    var example = document.getElementById('example');
+    var template = document.getElementById('alert-template').innerHTML;
+
+    title.disabled = true;
+    desc.disabled = true;
+    example.innerHTML = template;
+
+}
